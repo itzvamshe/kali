c94802f8012a31dc17b7964c88083d0cdc0340d0
itzvamshe <vamshe12@gmail.com>
2026-08-04 07:18:48 -0400

    added bit.py

---
+++ practice/bit.py
@@ -0,0 +1,15 @@
+# what is a bit ? -> it is the smallest unit in the data.
+
+# problems (in order)
+# 1. check if a number is even or odd.
+# 2. find the ith bit of a number.
+# 3. set the ith bit.
+# 4. clear the ith bit.
+# 5. toggle the ith bit.
+# 6. count the number of set bits.
+# 7. check if a number is a power of 2.
+# 8. find the unique number in an array where every other number appears twice.
+
+
+
+
